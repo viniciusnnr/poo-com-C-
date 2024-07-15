fe9d5306ac8aeecc5cbca0aec93399a2aca17276
Vinícius Santos <57609973+viniciusnnr@users.noreply.github.com>
2024-07-15 04:28:56 +0000

    cod_modular

---
+++ cod_modularizado/prog2.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  public class Car
+  {
+    public string model;
+    public string color;
+    public int year;
+    public void fullThrottle()
+    {
+      Console.WriteLine("The car is going as fast as it can!"); 
+    }
+  }
+}
