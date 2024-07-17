6b425ce9c47dbf983ab39ef72d1715209dc6ae34
Vinícius Santos <57609973+viniciusnnr@users.noreply.github.com>
2024-07-17 03:33:54 +0000

    pronto

--- 015 propriedade.cs
+++ 015 propriedade.cs
@@ -1,10 +1,19 @@
 class Person
 {
   private string name; // field
-
   public string Name   // property
   {
-    get { return name; }   // get method
-    set { name = value; }  // set method
+    get { return name; }
+    set { name = value; }
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Person myObj = new Person();
+    myObj.Name = "Liam";
+    Console.WriteLine(myObj.Name);
   }
 }
